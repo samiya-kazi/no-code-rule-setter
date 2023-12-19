9b7230b448144bd50c076d53d18aad9b7c883125
Samiya Kazi <samiya.kazi09@gmail.com>
2023-12-19 17:15:23 +0600

    feat: added basic navbar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { OptionCardComponent } from './components/option-card/option-card.component';
 import { BaseRuleComponent } from './components/base-rule/base-rule.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, OptionCardComponent, BaseRuleComponent],
+  imports: [CommonModule, RouterOutlet, OptionCardComponent, BaseRuleComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ client/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [NzTypographyModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
