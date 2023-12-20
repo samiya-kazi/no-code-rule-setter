766b2703ea4eb6aa668417ed7dbec793db92e685
Samiya Kazi <samiya.kazi09@gmail.com>
2023-12-20 15:48:35 +0600

    chore: added base rules styling

--- client/src/app/components/option-card/option-card.component.ts
+++ client/src/app/components/option-card/option-card.component.ts
@@ -1,14 +1,30 @@
 import { Component, Input } from '@angular/core';
-import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-option-card',
   standalone: true,
-  imports: [NzCardModule],
+  imports: [NzIconModule],
   templateUrl: './option-card.component.html',
   styleUrl: './option-card.component.css'
 })
 export class OptionCardComponent {
 
   @Input() title!: string;
+  @Input() index?: string;
+
+  getIcon () {
+    switch (this.title.toLowerCase()) {
+      case "vip":
+        return '../../../assets/svg/vip-gem.svg';
+      case "delivery":
+        return '../../../assets/svg/scooter.svg';
+      default:
+        return '../../../assets/svg/food-tray.svg';
+    }
+  }
+
+  getListIndex () {
+    return this.index ? Number(this.index) + 1 : null;
+  }
 }
